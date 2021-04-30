ba2be1639f08c411559268c495f838d4870523a9
Anubhav_dash <83328033+Anubhav-Dash@users.noreply.github.com>
2021-04-30 15:36:42 +0530

    Update main(2).py

--- main(2).py
+++ main(2).py
@@ -1,12 +1,9 @@
 # Prime factorisation
 Var = int(input("Insert the number you want to factorise: "))
 factors = []
-for i in range(2, Var-1):
-    for a in range(2, i):
-        if i%a==0:
-            break
-    else:
-        if Var%i==0:
+for i in range(1, Var):
+        x = Var%i
+        if x==0:
             factors.append(i)
 print("The Prime factors are :", factors)
 input()
